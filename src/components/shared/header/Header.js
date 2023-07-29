212e324221dff4f05421bbdda7d81d72a45c1413
Mir entafaz Ali <77488447+iamentafaz@users.noreply.github.com>
2023-07-29 12:37:05 +0530

    Menu dropdown for user added

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -13,20 +13,31 @@ import SignUpForm from '../../forms/SignUpForm';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { sagaActions } from '../../../redux/redux-saga/sagaActions';
-import { Avatar } from '@mui/material';
+import { Avatar, ListItemIcon, Menu, MenuItem } from '@mui/material';
+import { Logout, AccountCircle } from '@mui/icons-material';
 
 function Header() {
     const [loginOpen, setLoginOpen] = useState(false);
     const [openSignUpForm, setSignUpForm] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+
     const history = useHistory();
     const dispatch = useDispatch();
     const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
     const user = useSelector((state) => state.user);
     console.log(user);
+
     const loggedOut = () => {
         localStorage.removeItem('user_id');
         history.replace('/');
         dispatch({ type: sagaActions.LOGOUT_USER });
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
@@ -62,16 +73,40 @@ function Header() {
                             </Typography>
                             {isAuthenticated ? (
                                 <>
-                                    <Button
-                                        color="inherit"
-                                        onClick={loggedOut}
-                                        sx={{ fontWeight: '600' }}
+                                    <IconButton onClick={handleMenu}>
+                                        <Avatar sx={{ bgcolor: '#1e3762' }}>
+                                            M
+                                        </Avatar>
+                                    </IconButton>
+                                    <Menu
+                                        sx={{ mt: '45px' }}
+                                        id="menu-appbar"
+                                        keepMounted
+                                        anchorEl={anchorEl}
+                                        anchorOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        open={Boolean(anchorEl)}
+                                        onClose={handleClose}
                                     >
-                                        Logout
-                                    </Button>
-                                    <Avatar sx={{ bgcolor: '#1e3762' }}>
-                                        M
-                                    </Avatar>
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <AccountCircle fontSize="small" />
+                                            </ListItemIcon>
+                                            My Profile
+                                        </MenuItem>
+                                        <MenuItem onClick={loggedOut}>
+                                            <ListItemIcon>
+                                                <Logout fontSize="small" />
+                                            </ListItemIcon>
+                                            Logout
+                                        </MenuItem>
+                                    </Menu>
                                 </>
                             ) : (
                                 <Button
